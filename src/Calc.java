bcc6672572c6b287e877289f68ba4eb9b6acca53
zvu <zvu@agsr.by>
2023-10-16 12:06:45 +0300

    new calss Calc

---
+++ src/Calc.java
@@ -0,0 +1,9 @@
+public class Calc {
+    public static String sum(String a, String b) {
+        int numberA=Integer.parseInt(a);
+        int numberB=Integer.parseInt(b);
+        int resultInt=numberA+numberB;
+        String result = Integer.toString(resultInt);
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Calc.sum("23", "17"));
+    }
+}
